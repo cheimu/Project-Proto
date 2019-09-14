a7281c40ec321ed7447c584c7be9ef04915c7712
KMT051 <54625642+KMT051@users.noreply.github.com>
2019-09-14 16:36:26 +0800

    Add files via upload

--- Multi_Control.ino
+++ Multi_Control.ino
@@ -4,69 +4,51 @@
   #define SERIAL SerialUSB
 #else
   #define SERIAL Serial
-#endif
+#endif  //调用can总线库
 
-unsigned char stmp[8] = {0xff, 0x01, 0x00, 0x64, 0x00, 0x80, 0, }; //8段控制代码
-unsigned char bin[16];  //存放16位的二进制代码
+                      //总机地址       留用        留用     目标位置高低八位
+unsigned char stmp[8] = {0xff, 0x01, 0x00, 0x64, 0x00, 0x80, 0, 0}; //8段控制代码
+                      //       功能码      加速度       目标速度
+
+unsigned char buf[2]; //高低八位存放
 
 int posIni0;
-int posDifPrev0;
 int posDif0;
 
 int posIni1;
-int posDifPrev1;
 int posDif1;
 
-MCP_CAN CAN0(10);
-MCP_CAN CAN1(9);
+MCP_CAN CAN(10);
 
 void setup() {
   SERIAL.begin(9600); //串口波特率
-  CAN0.begin(CAN_1000KBPS);
-  CAN1.begin(CAN_1000KBPS);
+  CAN.begin(CAN_1000KBPS);  //can总线速率（只支持1000kbps）
   
-  posIni0 = analogRead(A0);
+  posIni0 = analogRead(A0);  //初始化关节角度传感器
   posIni1 = analogRead(A1);
   
-  CAN0.sendMsgBuf(0x01, 0, 8, stmp); //执行电机归中
-  CAN1.sendMsgBuf(0x01, 0, 8, stmp); //执行电机归中
+  CAN.sendMsgBuf(0x01, 0, 8, stmp); //执行电机归零
+  CAN.sendMsgBuf(0x02, 0, 8, stmp);
 }
 
 void loop() {
-  posDif0 = analogRead(A0) - posIni0;
-  if (abs(posDifPrev0 - posDif0) > 2) {
-    dec2bin(128 + posDif0 / 8);
-    stmp[7] = bin2dec(8, 15);
-    stmp[8] = bin2dec(0, 7);
-    CAN0.sendMsgBuf(0x01, 0, 8, stmp);
-    posDifPrev0 = posDif0;
-  }
-  delay(10);
-  
-  posDif1 = analogRead(A1) - posIni1;
-  if (abs(posDifPrev1 - posDif1) > 2) {
-    dec2bin(128 + posDif1 / 8);
-    stmp[7] = bin2dec(8, 15);
-    stmp[8] = bin2dec(0, 7);
-    CAN1.sendMsgBuf(0x01, 0, 8, stmp);
-    posDifPrev1 = posDif1;
-  }
-  delay(10);
+  posDif0 = analogRead(A0) - posIni0;  //取差值
+  spin(posDif0);
+  delay(10);  //等待信号广播完成
 }
 
-void dec2bin(int num) {  //十进制转二进制并存入bin
-  for (int i = 15; i >= 0; i--) {
-    bin[i] = num % 2;
-    num = num / 2;
-  }
+void dec2bin(int num) {  //转化高低八位存放于buf中
+  buf[0] = (num >> 8) & 0xFF;
+  buf[1] = num & 0xFF;
 }
 
-int bin2dec(int INI, int TER) { //二进制转十进制
-  int b2d = 0;
-  int power = 0;
-  for (int i = TER; i >= INI; i--) {
-    b2d += bin[i] * int(pow(2, power)*100+1)/100; //解决精度问题
-    power++;
+void spin(int posDif) {
+  if (abs(posDif) > 2) {  //设置传感器波动区间为±2
+    dec2bin(posDif0 * 35);  //将1024（103电位器为1024，霍尔角度传感器为4096） 转化为36000
+    
+    stmp[6] = buf[1];  //更改目标位置
+    stmp[7] = buf[0];
+    
+    CAN.sendMsgBuf(0x01, 0, 8, stmp);  //发送数据
   }
-  return b2d;
 }
